88b7c0e130c9aa290623b334a38e147ff31bd486
riqalter <riqalter@gmail.com>
2025-06-10 10:02:43 +0700

    api endpoint refactor & add dark theme on api docs (swagger ui)
    Signed-off-by: riqalter <riqalter@gmail.com>

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, APIRouter
+from fastapi.openapi.docs import get_swagger_ui_html
+from cache import get_cached_response, cache_response
 from models import CraftingRequest, CraftingResponse
 from ai import generate_craft
-from cache import get_cached_response, cache_response
-
+import fastapi_swagger_dark as fsd
 import json
 
 app: FastAPI = FastAPI(
@@ -12,10 +13,20 @@ app: FastAPI = FastAPI(
     swagger_ui_parameters={
         ""
         "syntaxHighlight": {"theme": "obsidian"},
-    }
+    },
+    docs_url=None
 )
 
-@app.post("/craft", response_model=CraftingResponse)
+router: APIRouter = APIRouter(
+    prefix="/api/v1",
+    tags=["Crafting"]
+)
+app.include_router(router)
+
+# penghitaman swagger ui
+fsd.install(app, path="/api/docs")
+
+@app.post("/api/v1/craft", response_model=CraftingResponse, tags=["Crafting"])
 def combine_elements(req: CraftingRequest) -> CraftingResponse:
     cached = get_cached_response(req.first_element, req.second_element)
     if cached:
@@ -40,9 +51,9 @@ def combine_elements(req: CraftingRequest) -> CraftingResponse:
 @app.get("/", tags=["Root"])
 def read_root() -> dict[str, str]:
     return {
-        "message": "Visit /docs for API documentation."
+        "message": "Visit /api/docs for API documentation."
     }
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, use_colors=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, use_colors=True, reload=True)
